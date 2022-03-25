b221ff9fdf895ed50fd6a6be22e986339f20f6d8
coltonxstone <54927184+coltonxstone@users.noreply.github.com>
2022-03-25 14:30:07 -0700

    Got checkboxes for GPA working

--- filter.js
+++ filter.js
@@ -1,13 +1,13 @@
 //http://jsfiddle.net/6wYzw/41/
 //https://stackoverflow.com/questions/8796472/filtering-with-checkboxes-using-jquery
-
+var requirements = {}
+var gpas = {}
 async function createFilter(){
   /*creates fellowship dictionary with each fellowship name 
   as the key and then attaches the fellowship 
   information to each fellowship name (like a hash table)
   Does NOT display on page
   */
-  var requirements = {}
   requirements_json_data.forEach(function(item) {
   if(!(item.name in requirements)){
     requirements[item.name] = [item] // adds new fellowship name to array
@@ -19,7 +19,7 @@ async function createFilter(){
   cleanRequirements();
   function cleanRequirements(){
     var html = $.map(requirements, function(body, fellowship) {
-      console.log(fellowship);
+      //console.log(fellowship);
       return $.map(body, function(item, i) {
             return $.map(item, function(line, key){
                 //console.log(key)
@@ -32,7 +32,15 @@ async function createFilter(){
         })
       })
   }
-  //console.log(requirements);
+  // mapping for gpas of all fellowships to ease filtering
+  // puts all the gpas into an array for easier comparisons
+   gpas = $.map(requirementList, function(item){
+    if(!item.min_gpa){ // if gpa is null just set it to 0
+      item.min_gpa = 0.0
+    }
+     return item.min_gpa
+   })
+  //console.log(gpas)
 }
 
 //https://stackoverflow.com/questions/54695113/multi-condition-filtering-with-checkboxes-javascript
@@ -59,4 +67,23 @@ function removePanel(a) {
   return false;
 }
 
-
+function onCheck(event) {
+  // get element that was clicked on (the checkbox)
+  var tempElem = event.target
+  // for each element in the accordion
+  for (var i = 0; i < element.length; i++) {
+    // if checkbox is checked
+    if(tempElem.checked == true){
+    // check the stored gpa of the element, see if its larger than the checkbox value or null
+    if (gpas[i] >= tempElem.value.toLowerCase() || gpas[i] == 0) {
+        element[i].style.display = "block"
+     } else {
+        element[i].style.display = "none"
+      }
+    }
+      // restore all of the accordion elements if the box gets unchecked
+      if(tempElem.checked == false){
+        element[i].style.display = "block"
+      }
+    }
+  }
